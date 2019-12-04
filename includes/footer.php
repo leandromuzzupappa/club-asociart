82ec3b3be6a35f22bae603ced7938ade3a289069
Leandro Muzzupappa <leann.muzzu@gmail.com>
2019-12-04 10:07:14 -0300

    Iniciando proyecto

---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+<script src="assets/scripts/particles.min.js"></script>
+<script src="assets/scripts/app.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,45 @@
+<?php include 'includes/head.php';?>
+<div page="inicio"></div>
+
+<header id="particles-js">
+    <div class="container">
+
+        <div class="col logos">
+            <img src="assets/images/logo-club-asociart-blanco.svg" alt="Club Asociart">
+            <img src="assets/images/logo-asociart-blanco.svg" alt="Asociart">
+        </div>
+
+        <div class="bajada">
+            <h1>
+                Bienvenido
+            </h1>
+            <p>
+                Al programa de recompensas que premia a sus <br>
+                Clientes y Productores Asesores de Seguros.
+            </p>
+            <div id="scrollDown">
+                <svg viewBox="0 0 62 90">
+                <g id="Flechita">
+                    <path id="flechita_x5F_contorno" class="st0" d="M31,87.8L1.6,58.4c-0.7-0.8-0.6-2.1,0.2-2.8c0.7-0.6,1.9-0.6,2.6,0L31,82.2
+                        l26.6-26.6c0.8-0.7,2.1-0.6,2.8,0.2c0.6,0.7,0.6,1.9,0,2.6L31,87.8z"/>
+                </g>
+                <g id="Mouse">
+                    <path id="mouse_x5F_contorno" class="st0" d="M34,45h-6c-6.6,0-12-5.4-12-12V14.5c0-6.6,5.4-12,12-12h6c6.6,0,12,5.4,12,12V33
+                        C46,39.6,40.6,45,34,45z M28,6.5c-4.4,0-8,3.6-8,8V33c0,4.4,3.6,8,8,8h6c4.4,0,8-3.6,8-8V14.5c0-4.4-3.6-8-8-8H28z"/>
+                    <path id="mouse_x5F_boton" class="st0" d="M31,22c-1.1,0-2-0.9-2-2v-7c0-1.1,0.9-2,2-2c1.1,0,2,0.9,2,2v7C33,21.1,32.1,22,31,22z"
+                        />
+                </g>
+                </svg>
+
+            </div>
+        </div>
+
+    </div>
+</header>
+<main>
+    <section class="choseProfile">
+
+    </section>
+</main>
+
+<?php include 'includes/footer.php'?>
